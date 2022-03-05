eb8bea9dfecc25ec62e5131e60890bce12374faf
Andarael <josuraad@gmail.com>
2022-03-06 00:55:29 +0100

    added mutation variation function, refator newgen with evolve, mate, and mutate

--- genetics.py
+++ genetics.py
@@ -21,22 +21,19 @@ def newGen(population, population_size, mutation_rate, data):
         nbchild : number of children to produce
         mutation_rate : Percent of genes to modify
     """
-    children = []
     # population_size, nb_parents_kept, children = keep_parents(population, keep)
+    breed_per_parents, honeyMoon = get_mating_pool(population, population_size)
+    children = []
+    # todo find a betterway to breed parents when nb_parents or nb_children is small (avoid out of range)
+    return evolve(population, population_size, mutation_rate, data, children, breed_per_parents, honeyMoon)
 
-    breed_per_parents = int(np.ceil(population_size / len(population)))  # nb de baise/parents
-    honeyMoon = []  # tableau de chasse de chaque parent
+    # for c in children:
+    # c.genome = mutation(c.genome, mutation_rate)
+    # c.age += 1
 
-    for i in range(len(population)):
-        tmp = np.arange(0, len(population))    # tous les parents possible
-        tmp = np.delete(tmp, i)                # un parents ne se baise pas lui meme
-        # randomise le tout - todo check si nécéssaire (plus rapide avec shuffle, what ? )
-        np.random.shuffle(tmp)
-        honeyMoon.append(tmp[:breed_per_parents])         # prend les premiers parents
-    honeyMoon = (np.array(honeyMoon))
 
-    # todo find a betterway to breed parents when nb_parents && nb_children is small
-    for c in range(population_size):
+def evolve(population, population_size, mutation_rate, data, children, breed_per_parents, honeyMoon):
+    for c in (range(population_size)):
         i = c // breed_per_parents
         j = c % breed_per_parents
         g1 = population[i].genome
@@ -44,13 +41,20 @@ def newGen(population, population_size, mutation_rate, data):
         new_genome = crossover(g1, g2)
         new_genome = mutation(new_genome, mutation_rate)
         children.append(Individu(new_genome, data))
-    np.random.shuffle(children)
-
-    # for c in children:
-    # c.genome = mutation(c.genome, mutation_rate)
-    # c.age += 1
-
     return children
+
+
+def get_mating_pool(population, population_size):
+    breed_per_parents = int(np.ceil(population_size / len(population)))  # nb de baise/parents
+    honeyMoon = []  # tableau de chasse de chaque parent
+    for i in range(len(population)):
+        tmp = np.arange(0, len(population))    # tous les parents possible
+        tmp = np.delete(tmp, i)                # un parents ne se baise pas lui meme
+        # randomise le tout - todo check si nécéssaire (plus rapide avec shuffle, what ? )
+        np.random.shuffle(tmp)
+        honeyMoon.append(tmp[:breed_per_parents])         # prend les premiers parents
+    honeyMoon = (np.array(honeyMoon))
+    return breed_per_parents, honeyMoon
 
 
 def keep_parents(population, keep):
@@ -95,20 +99,33 @@ def crossover(g1, g2):
 
 
 def mutation(genome, mutation_rate: float):
+    nb_genes = len(genome)-1
+    for index1 in range(len(genome)):
+        if(rd.random() < mutation_rate):
+            index2 = rd.randint(0, nb_genes)
+            genome[index1], genome[index2] = genome[index2], genome[index1]
+    return genome
+
+
+def mutation2(genome, mutation_rate: float):
+    nb_genes = len(genome)-1
+    for _ in range(nb_genes):
+        if(rd.random() < mutation_rate):
+            index1 = rd.randint(0, nb_genes)
+            index2 = rd.randint(0, nb_genes)
+            genome[index1], genome[index2] = genome[index2], genome[index1]
+    return genome
+
+
+def mutation3(genome, mutation_rate: float):
     if(rd.random() > mutation_rate):
         return genome
     nb_genes = len(genome)-1
-    mutation_rate = np.clip(mutation_rate, 0.0, 1.0)
     nb_mutations = int(np.ceil(nb_genes * rd.random()))
-
     for _ in range(nb_mutations):
-        index1 = rd.randint(0, nb_genes)  # gene to mutate (index)
-        index2 = rd.randint(0, nb_genes)  # gene to mutate (index)
-        gene1 = genome[index1]
-        gene2 = genome[index2]
-        genome[index2] = gene1
-        genome[index1] = gene2
-
+        index1 = rd.randint(0, nb_genes)
+        index2 = rd.randint(0, nb_genes)
+        genome[index1], genome[index2] = genome[index2], genome[index1]
     return genome
 
 
@@ -123,8 +140,6 @@ if __name__ == '__main__':
     population[8].age = 8
     population[6].age = 8
     population[7].age = 8
-
-    population = newGen(population, .2, 0.02, dm.data)
 
     gene = population[0].genome
     print("==>> population[0].genome: ", gene)
