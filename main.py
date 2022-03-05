4d8551bfb594b4799cc3eb2f2eeeb7750863b194
Andarael <josuraad@gmail.com>
2022-03-06 00:54:57 +0100

    added target loss, print progress

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from genetics import *
 import numpy as np
 
 
-epoch = 2000
+max_epoch = 8000
 population_size = 100
 keep = 0.2
-mutation_rate = 0.0
-# add nb of block for mutations
+mutation_rate = 0.003
+target_loss = 3.15
+# add max nb of block for mutations ?
 # todo longévité des individus (ne pas tuer tous les individus à chaque epoch (cf tp)) (enfaitnon c'est pas top après avoir testé ... )
 
 
@@ -28,12 +29,17 @@ def run():
     theOne = population[0]
 
     print("Genetic evolution in progress ...")
-    for _ in tqdm(range(epoch)):
+    i = 1
+    #todo find a way to bring back tqdm with custom info & break when at taget loss
+    while( i <= max_epoch and loss[-1] > target_loss):
         population = newGen(population[:int(population_size * keep)], population_size, mutation_rate, dm.data)
         population = sorted(population, key=lambda individu: individu.loss)
         loss.append(population[0].loss)
         if theOne.loss > population[0].loss:
             theOne = population[0]
+        if(i%10 ==0):
+            print("Epoch :", i, "- loss :", loss[-1])
+        i += 1
 
     # todo update matplotlib per n iteration
     print("best loss =", theOne.loss)
