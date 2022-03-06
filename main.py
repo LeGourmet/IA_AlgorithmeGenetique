a2252f9c3505199d6d07514845f83f19f9db67f8
Andarael <josuraad@gmail.com>
2022-03-06 22:03:51 +0100

    updated plot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import final
 from tqdm import tqdm
 from data_manager import *
 from view_manager import *
@@ -9,10 +10,11 @@ import numpy as np
 max_epoch = 2000
 population_size = 100
 keep = 0.2
-mutation_rate = 0.006
+mutation_rate = 0.007
 target_loss = 3.14
 data_size = 50
-refresh = 200
+refresh = 20
+show_progress = True  # real time plot, but slower
 
 
 def initialize_population(dm, population, losses):
@@ -25,11 +27,21 @@ def initialize_population(dm, population, losses):
     return theOne
 
 
-def display_evolution(vm, dm, losses, theOne):
-    print("best loss =", theOne.loss, "at epoch ", len(losses) - 1)
-    best_gene = np.array(theOne.genome)
+def display_evolution_graph(vm, dm, losses, last_gene, final=False):
+    best_gene = np.array(last_gene.genome)
     best_path = dm.data[best_gene]
-    vm.update(losses, best_path)
+    if show_progress:
+        vm.update(losses, best_path)
+    if final:
+        vm.update(losses, best_path, final=True)
+
+
+def disaply_evolution_info(vm, dm, population, losses, theOne, epoch):
+    if(epoch % (max_epoch // 10) == 0):  # update every 10% of progress
+        print("\nEpoch :", epoch, "- loss :", losses[-1])
+        print("\nBest loss =", theOne.loss, "at epoch ", len(losses) - 1)
+    if(epoch % refresh == 0):
+        display_evolution_graph(vm, dm, losses, population[0])
 
 
 def run_genetic(vm, dm, population, losses, theOne):
@@ -38,14 +50,18 @@ def run_genetic(vm, dm, population, losses, theOne):
         elite = population[:int(population_size * keep)]
         population = newGen(elite, population_size, mutation_rate, dm.data)
         population = sort_population(population)
-        losses.append(population[0].loss)
-        if theOne.loss > population[0].loss:
-            theOne = population[0]
-        if(epoch % (max_epoch // 10) == 0):
-            print("\nEpoch :", epoch, "- loss :", losses[-1])
-            display_evolution(vm, dm, losses, theOne)
+        theOne = update_loss(population, losses, theOne)
+        disaply_evolution_info(vm, dm, population, losses, theOne, epoch)
         if(losses[-1] < target_loss):
             break
+    print("\nFinished genentic evolution with best loss =", theOne.loss, "at epoch ", len(losses) - 1)
+    return theOne
+
+
+def update_loss(population, losses, theOne):
+    losses.append(population[0].loss)
+    if theOne.loss > population[0].loss:
+        theOne = population[0]
     return theOne
 
 
@@ -73,7 +89,7 @@ def run():
 
     theOne = initialize_population(dm, population, losses)
     theOne = run_genetic(vm, dm, population, losses, theOne)
-    display_evolution(vm, dm, losses, theOne)
+    display_evolution_graph(vm, dm, losses, theOne, final=True)
 
 
 if __name__ == '__main__':
--- view_manager.py
+++ view_manager.py
@@ -1,24 +1,32 @@
 import matplotlib.pyplot as plt
+from numpy import block
+
+from data_manager import DataManager
 
 
 class ViewManager:
     def __init__(self):
-        plt.ion()
-        self.figure, axes = plt.subplots(1, 2)
+        # plt.ion()
+        self.figure, axes = plt.subplots(1, 2, figsize=(10, 4))
+        axes[0].set_aspect(1)
+
         self.fig_path = axes[0]
         self.fig_loss = axes[1]
+        # plt.tight_layout()
 
-    def update(self, loss, data):
+    def update(self, losses, best_path, final=False):
         self.fig_path.clear()
         self.fig_loss.clear()
-        for i in range(len(data)):
-            self.fig_path.plot([data[i - 1][0], data[i][0]], [data[i - 1][1], data[i][1]])
-        for point in data:
+        for i in range(len(best_path)):
+            self.fig_path.plot([best_path[i - 1][0], best_path[i][0]], [best_path[i - 1][1], best_path[i][1]])
+        for point in best_path:
             self.fig_path.plot(point[0], point[1], marker='o')
 
         self.fig_loss.set_xlabel('Epoch')
         self.fig_loss.set_ylabel('Loss')
-        self.fig_loss.plot(loss)
-        plt.pause(0.1)
+        self.fig_loss.plot(losses)
 
-        plt.tight_layout()
+        if final:
+            plt.show(block=True)
+        else:
+            plt.pause(0.01)
