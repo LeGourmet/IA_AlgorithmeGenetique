5ee532ef359c90ce3e7dc8a792550d13aa2c152d
Andarael <josuraad@gmail.com>
2022-03-06 01:01:35 +0100

    reformat loop

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import numpy as np
 max_epoch = 8000
 population_size = 100
 keep = 0.2
-mutation_rate = 0.003
-target_loss = 3.15
+mutation_rate = 0.001
+target_loss = 4
 # add max nb of block for mutations ?
 # todo longévité des individus (ne pas tuer tous les individus à chaque epoch (cf tp)) (enfaitnon c'est pas top après avoir testé ... )
 
@@ -29,17 +29,17 @@ def run():
     theOne = population[0]
 
     print("Genetic evolution in progress ...")
-    i = 1
-    #todo find a way to bring back tqdm with custom info & break when at taget loss
-    while( i <= max_epoch and loss[-1] > target_loss):
-        population = newGen(population[:int(population_size * keep)], population_size, mutation_rate, dm.data)
+    for epoch in tqdm(range(max_epoch)):
+        elite = population[:int(population_size * keep)]
+        population = newGen(elite, population_size, mutation_rate, dm.data)
         population = sorted(population, key=lambda individu: individu.loss)
         loss.append(population[0].loss)
         if theOne.loss > population[0].loss:
             theOne = population[0]
-        if(i%10 ==0):
-            print("Epoch :", i, "- loss :", loss[-1])
-        i += 1
+        if(epoch % (max_epoch//10) == 0):
+            print("\nEpoch :", epoch, "- loss :", loss[-1])
+        if(loss[-1] < target_loss):
+            break
 
     # todo update matplotlib per n iteration
     print("best loss =", theOne.loss)
